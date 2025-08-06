050baf0898ea1201da6d93093eabfdd7d1570df9
Plean Somjit <plean_som@cmu.ac.th>
2025-08-07 06:59:36 +0700

    lab4_3.7fix

--- 331-Lab02-Intro-to-vite/src/router/index.ts
+++ 331-Lab02-Intro-to-vite/src/router/index.ts
@@ -10,6 +10,7 @@ import NotFoundView from '@/views/NotFoundView.vue'
 import NetworkErrorView from '@/views/NetworkErrorView.vue'
 import nProgress from 'nprogress'
 import EventService from '@/services/EventServices'
+import { useEventStore } from '@/stores/event'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
