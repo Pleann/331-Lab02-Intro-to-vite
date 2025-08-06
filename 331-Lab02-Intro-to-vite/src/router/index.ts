ed7e919fff3b8341c3e63e8671e4bd47dc0384ab
Plean Somjit <plean_som@cmu.ac.th>
2025-08-07 05:20:46 +0700

    lab3_5

--- 331-Lab02-Intro-to-vite/src/router/index.ts
+++ 331-Lab02-Intro-to-vite/src/router/index.ts
@@ -7,6 +7,7 @@ import EventRegisterView from '@/views/events/RegisterView.vue'
 import EventEditView from '@/views/events/EditView.vue'
 import EventLayoutView from '@/views/events/LayoutView.vue'
 import NotFoundView from '@/views/NotFoundView.vue'
+import NetworkErrorView from '@/views/NetworkErrorView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -58,6 +59,11 @@ const router = createRouter({
       name: '404-resource-view',
       component: NotFoundView,
       props: true
+    },
+    {
+      path: '/network-error',
+      name: 'network-error-view',
+      component: NetworkErrorView
     },
     {
       path: '/:catchAll(.*)',
